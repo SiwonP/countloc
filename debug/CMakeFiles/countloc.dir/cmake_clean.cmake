1c68bc3b6230994f4d4876f9e68a1d367cabf2c6
Simon Petit <nomisp96@hotmail.fr>
2018-02-26 16:00:27 +0100

    initial commit

---
+++ debug/CMakeFiles/countloc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "lex.c"
+  "CMakeFiles/countloc.dir/lex.c.o"
+  "countloc.pdb"
+  "countloc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/countloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
